0ad2e1628afa233d836fc94aa7de870ca310eae7
Objeto-a-Sujeto <100564433@alumnos.uc3m.es>
2025-12-12 13:33:43 +0100

    Create  next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compress: true,
+  poweredByHeader: false,
+  trailingSlash: false,
+  skipTrailingSlashRedirect: true,
+};
+
+module.exports = nextConfig;
